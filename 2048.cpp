ef4e68cd9ac4b8d8667def39a82d61d430905b27
Wouter Vermaelen <vermaelen.wouter@gmail.com>
2014-04-07 14:22:58 +0200

    Tweak score_move_node()
    Conflicts:
    	2048.cpp

--- 2048.cpp
+++ 2048.cpp
@@ -268,7 +268,7 @@ static const int CACHE_DEPTH_LIMIT  = 6;
 static const int SEARCH_DEPTH_LIMIT = 8;
 
 static float score_move_node(eval_state &state, board_t board, float cprob) {
-    if(cprob < state.cprob_thresh || state.curdepth >= SEARCH_DEPTH_LIMIT) {
+    if (cprob < state.cprob_thresh || state.curdepth >= SEARCH_DEPTH_LIMIT) {
         if(state.curdepth > state.maxdepth)
             state.maxdepth = state.curdepth;
         return score_heur_board(board);
@@ -282,23 +282,19 @@ static float score_move_node(eval_state &state, board_t board, float cprob) {
         }
     }
 
-    int move;
-    float best = 0;
-
+    float best = 0.0f;
     state.curdepth++;
-    for(move=0; move<4; move++) {
+    for (int move = 0; move < 4; ++move) {
         board_t newboard = execute_move(move, board);
         state.moves_evaled++;
-        if(board == newboard)
-            continue;
 
-        float res = score_tilechoose_node(state, newboard, cprob);
-        if(res > best)
-            best = res;
+        if (board != newboard) {
+            best = std::max(best, score_tilechoose_node(state, newboard, cprob));
+        }
     }
     state.curdepth--;
 
-    if(state.curdepth < CACHE_DEPTH_LIMIT) {
+    if (state.curdepth < CACHE_DEPTH_LIMIT) {
         state.trans_table[board] = best;
     }
 
